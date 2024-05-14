3c47b462ec173d7b29d3b1e5ffe0e1dd23612eb3
PirozhkovaElizaveta <zhenshina21@mail.ru>
2024-05-14 17:00:26 +0300

    add zoo staff

--- HomeWork.py
+++ HomeWork.py
@@ -38,3 +38,48 @@ def animal_sound(animals):
 
 animals = [Попугай, Корова, Змея]
 animal_sound(animals)
+
+class ZooKeeper:
+    def __init__(self, name):
+        self.name = name
+
+    def feed_animal(self, animal):
+        print(f"{self.name} кормит {animal.name}")
+
+class Veterinarian:
+    def __init__(self, name):
+        self.name = name
+
+    def heal_animal(self, animal):
+        print(f"{self.name} лечит {animal.name}")
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        print(f"{animal.name} добавлен в зоопарк")
+
+    def add_staff(self, staff_member):
+        self.staff.append(staff_member)
+        print(f"{staff_member.name} добавлен в зоопарк")
+
+popugai = Bird(name="Попугай", age=2, wing_span=25)
+korova = Mammal(name="Корова", age=5, fur_color="Golden")
+zmeya = Reptile(name="Змея", age=3, scale_type="Smooth")
+
+zoo = Zoo()
+zoo.add_animal(popugai)
+zoo.add_animal(korova)
+zoo.add_animal(zmeya)
+
+zoo_keeper = ZooKeeper(name="Иван")
+veterinarian = Veterinarian(name="Анна")
+
+zoo.add_staff(zoo_keeper)
+zoo.add_staff(veterinarian)
+
+zoo_keeper.feed_animal(popugai)
+veterinarian.heal_animal(korova)
